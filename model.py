a6f1b08e980dfec968e7a9bdd6685eb2b2f556ca
Eshan133 <ishan.maharjan5@gmail.com>
2025-08-14 11:27:11 +0545

    Adding Multihead Attention

--- model.py
+++ model.py
@@ -78,4 +78,65 @@ class FeedForwardBlock(nn.Module):
         return self.liner_2(self.dropout(torch.relu(self.linear_1(x))))
 
 
-def MultiHeadAAttention
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model:int, h:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        # ??????? what is happening here, why linear layer?
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) # Wq
+        self.w_k = nn.Linear(d_model, d_model) # Wk
+        self.w_v = nn.Linear(d_model, d_model) # Wv
+
+        self.w_o = nn.Linear(d_model, d_model) # Wo
+
+        self.dropout = nn.Dropout(dropout)
+    
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (Batch, h, seq_len, d_k) ---> (Batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k) # transpose interchange the last two dims.
+        if mask is not None:
+            attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) # (Batch, h, seq_len, seq_len)
+
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        # (attention_scores @ value) --> SHape = (Batch, h, seq_len, d_k)
+        return (attention_scores @ value), attention_scores
+
+
+    def forward(self, q, k, v, mask):
+        # mask for hiding attention for certain words
+
+        query = self.w_q(q) # Q` = (Batch, seq_len, d_model) ---> (Batch, seq_len, d_model)
+        key = self.w_q(k) #K`
+        value = self.w_q(v) #V`
+
+        # Seperating the heads 
+        # (Batch, seq_len, d_model) ---> (Batch, seq_len, h, d_k) --transpose-> (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1,2) # Splitting the embedding and not the sentence
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2) 
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2) 
+
+        # Calculating the attention
+        x, self.attention_score = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        # (Batch, h, seq_len, d_k) --> (Batch, seq_len, h, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+
+
+
+
+
